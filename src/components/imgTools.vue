6d93be20a5f89f62beb725e91384d5fbd7b4259c
mark <supachai.da@gmail.com>
2022-09-07 22:01:54 +0700

    simple program for calculate fault current in 012 domain

--- src/components/imgTools.vue
+++ src/components/imgTools.vue
@@ -1,10 +1,13 @@
-
 <template>
 <div class="input">
-<h5>Zf</h5><input v-model="xFault" type="h">
+<h5>Zf</h5><input v-model="rFault" type="Float"><h5>ohm&ensp;</h5>
+<h5>S (Base)</h5><input v-model="sBase" type="Float"><h5>MVA&ensp;</h5>
+<h5>V (Base)</h5><input v-model="vBase" type="Float"><h5>V&ensp;</h5>
 </div>
-<h3>Fault Current = {{this.faultCurrent}} pu</h3>
-<h3>largestNum = {{this.largestNum}}</h3>
+<h3>Fault Current (3 Phase) &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; I(1) = {{this.threePhaseFaultCurrent}} pu</h3>
+<h3>Fault Current (Single line to ground) I(1) = {{this.singleLineFaultCurrent}} pu&ensp;=&ensp;I(2)&ensp;=&ensp;I(0)</h3>
+<h3>Fault Current (Line to line)&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; I(1) = {{this.linetoLineFaultCurrent}}</h3>
+<h3>Fault Current (Double line to ground) I(1) = {{this.doubleLineFaultCurrent}}</h3>
     <div class="container">
   <div class="row">
     <div class="col-sm">
@@ -213,6 +216,8 @@
 const generatorUrl = require('../assets/img_generator.jpeg')
 const cableUrl = require('../assets/img_cable.jpeg')
 const transformerUrl = require('../assets/img_transformer.jpeg')
+const math = require('../../node_modules/mathjs/lib/browser/math')
+
 export default {
   name: 'ImgTools',
   mounted() {
@@ -223,12 +228,23 @@ export default {
       imgGen: generatorUrl,
       imgCable: cableUrl,
       imgTransformer: transformerUrl, 
-      x: [[0, parseFloat(0), 0], [0, parseFloat(0), 0], [0, parseFloat(0), 0], [0, parseFloat(0), 0], [0, parseFloat(0), 0]], 
-      xFault: 0,
+      x: [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+      rFault: 0,
       selectedRadio: 0,
+      sBase: 0,
+      vBase: 0
     }
   },
   computed: {
+    rFaultPu: {
+      get() {
+        let v = parseFloat(this.vBase)
+        let vv = math.pow(v,2)
+        let zBase = math.divide(vv,parseFloat(this.sBase))
+        let y = math.divide(parseFloat(this.rFault),zBase)
+        return y
+      }
+    },
     genPosition: {
       get() {
         let genPs = []
@@ -251,6 +267,28 @@ export default {
         return parseInt(largestN)
       }
     },
+     xTotalZero: {
+     get() {
+        let xTotal = 0
+        let xTotal1 = 0
+        let xTotal2 = 0
+        if(this.largestNum > this.selectedRadio) { // In case the system has more than two generators that the fault location is being between two of them
+        for(let i = 0; i < this.selectedRadio; i++) {
+          xTotal1 += parseFloat(this.x[i][0])
+        }
+         for(let i = this.selectedRadio; i <= this.largestNum; i++) {
+          xTotal2 += parseFloat(this.x[i][0])
+        }
+         xTotal = (parseFloat(xTotal1)*parseFloat(xTotal2))/(parseFloat(xTotal1)+parseFloat(xTotal2)) //sum resistor for each side in serie method, and after that sum both of them in parallel method
+         
+        }else { // In case the generator is situated before or after fault location
+          for(let i = 0; i < this.selectedRadio; i++) {
+          xTotal += parseFloat(this.x[i][0])
+        }
+        }
+        return parseFloat(xTotal).toFixed(2)
+    }
+  },
     xTotalPositive: {
      get() {
         let xTotal = 0
@@ -263,7 +301,7 @@ export default {
          for(let i = this.selectedRadio; i <= this.largestNum; i++) {
           xTotal2 += parseFloat(this.x[i][1])
         }
-         xTotal = (parseFloat(xTotal).toFixed(2)*parseFloat(xTotal2).toFixed(2))/(parseFloat(xTotal1).toFixed(2)+parseFloat(xTotal2).toFixed(2)) //sum resistor for each side in serie method, and after that sum both of them in parallel method
+         xTotal = (parseFloat(xTotal1)*parseFloat(xTotal2))/(parseFloat(xTotal1)+parseFloat(xTotal2)) //sum resistor for each side in serie method, and after that sum both of them in parallel method
          
         }else { // In case the generator is situated before or after fault location
           for(let i = 0; i < this.selectedRadio; i++) {
@@ -273,9 +311,71 @@ export default {
         return parseFloat(xTotal).toFixed(2)
     }
   },
-  faultCurrent: {
+  xTotalNegative: {
+     get() {
+        let xTotal = 0
+        let xTotal1 = 0
+        let xTotal2 = 0
+        if(this.largestNum > this.selectedRadio) { // In case the system has more than two generators that the fault location is being between two of them
+        for(let i = 0; i < this.selectedRadio; i++) {
+          xTotal1 += parseFloat(this.x[i][2])
+        }
+         for(let i = this.selectedRadio; i <= this.largestNum; i++) {
+          xTotal2 += parseFloat(this.x[i][2])
+        }
+         xTotal = (parseFloat(xTotal1)*parseFloat(xTotal2))/(parseFloat(xTotal1)+parseFloat(xTotal2)) //sum resistor for each side in serie method, and after that sum both of them in parallel method
+         
+        }else { // In case the generator is situated before or after fault location
+          for(let i = 0; i < this.selectedRadio; i++) {
+          xTotal += parseFloat(this.x[i][2])
+        }
+        }
+        return parseFloat(xTotal).toFixed(2)
+    }
+  },
+  threePhaseFaultCurrent: {
     get() {
-      return parseFloat(1/(this.xTotalPositive+parseFloat(this.xFault))).toFixed(2)
+      let x = math.complex(parseFloat(this.rFaultPu),parseFloat(this.xTotalPositive))
+      let y = math.divide(1,x)
+      return `${parseFloat(y.re).toFixed(2)} ${parseFloat(y.im).toFixed(2)}i`
+    }
+  },
+    singleLineFaultCurrent: {
+    get() {
+      let complex =  parseFloat(this.xTotalZero)+parseFloat(this.xTotalPositive)+parseFloat(this.xTotalNegative)
+      let real = parseFloat(this.rFaultPu)*3
+      let x = math.complex(real,complex)
+      let y = math.divide(1,x)
+      return `${parseFloat(y.re).toFixed(2)} ${parseFloat(y.im).toFixed(2)}i`
+    }
+  },
+    linetoLineFaultCurrent: {
+    get() {
+      let complex = parseFloat(this.xTotalPositive)+parseFloat(this.xTotalNegative)
+      let real = parseFloat(this.rFaultPu)
+      let x = math.complex(real,complex)
+      let y = math.divide(1,x)
+      let yNeg = math.subtract(0,y)
+      return `${parseFloat(y.re).toFixed(2)} ${parseFloat(y.im).toFixed(2)}i pu ,I(2) = ${parseFloat(yNeg.re).toFixed(2)} ${parseFloat(yNeg.im).toFixed(2)}i pu`
+    }
+  },
+   doubleLineFaultCurrent: {
+    get() {
+     let r = math.complex(parseFloat(this.rFaultPu)*3,parseFloat(this.xTotalZero))  // R(fault) + X(zero)
+     let rNegative = math.complex(0,parseFloat(this.xTotalNegative))
+     let rr = math.multiply(r,rNegative)  //  (R(fault) + X(zero)) * X(negative)
+     let rrr = math.add(r,rNegative)  // (R(fault) + X(zero)) + X(negative)
+     let rparallel = math.divide(rr,rrr)
+     let rPositive = math.complex(0,parseFloat(this.xTotalPositive))
+     let x = math.add(rPositive,rparallel)
+     let y = math.divide(1,x)
+     let rSecond = math.divide(r,rrr)
+     let iSecond = math.multiply(rSecond,y)
+     let z = math.subtract(0,iSecond)
+     let rZero = math.divide(rNegative,rrr)
+     let iZero = math.multiply(rZero,y)
+     let zz = math.subtract(0,iZero)
+    return `${parseFloat(y.re).toFixed(2)} ${parseFloat(y.im).toFixed(2)}i pu ,I(2) = ${parseFloat(z.re).toFixed(2)} ${parseFloat(z.im).toFixed(2)}i pu ,I(0) = ${parseFloat(zz.re).toFixed(2)} ${parseFloat(zz.im).toFixed(2)}i pu`
     }
   }
   }
